75c255c0df14e8262050037d3158708d98327d77
Tino Thamjarat <lynx.tno@gmail.com>
2014-11-18 03:22:18 +0700

    create boolean for login

--- addItem.php
+++ addItem.php
@@ -8,22 +8,26 @@
         <?php 
             include_once 'common/header.php';
         ?>
-        <form role ="form">
+        <form role ="form" action ="addConfirm.php" method = "POST">
             <div class="form-group">
                 <label for="inputTask">Task Name</label>
-                <input type ="text" class="form-control" id ="taskName" placeholder="your task...">
+                <input type ="text" class="form-control" name ="taskName" placeholder="your task...">
             </div>
             <div class="form-group">
                 <label for="inputTask">Course Name</label>
-                <input type ="text" class="form-control" id ="taskName" placeholder="course name...">
+                <input type ="text" class="form-control" name ="courseName" placeholder="course name...">
             </div>
              <div class="form-group">
-                <label for="inputTask">Deadline</label>
-                <input type ="text" class="form-control" id ="taskName" placeholder="deadline...">
+                <label for="inputTask">Deadline(YYYY/MM/DD)</label>
+                <input type ="text" class="form-control" name ="deadline" placeholder="deadline...">
             </div>
             <div class="form-group">
                 <label for="inputTask">Status</label>
-                <input type ="text" class="form-control" id ="taskName" placeholder="status...">
+                <input type ="text" class="form-control" name ="status" placeholder="status...">
+            </div>
+            <div class="form-group">
+                <input type ="submit" class="btn btn-success" value ="Add to list">
+                <a class = "btn btn-danger" href = "index.php">Cancel</a>
             </div>
         </form>
     </body>
