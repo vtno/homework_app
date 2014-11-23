089a93abc71b6a5b83c6dc066502976757e5bd45
Tino Thamjarat <lynx.tno@gmail.com>
2014-11-24 00:22:33 +0700

    fix regis redirect bug

--- regisConfirm.php
+++ regisConfirm.php
@@ -12,7 +12,7 @@ $password = $_POST['password'];
 $addUser = "INSERT INTO user(username,password)VALUES('" . $username . "','" . $password . "') ";
 if ($cn->query($addUser)) {
     echo "<h1>DONE!<small> Your profile have been created. Please enter your information.</small></h1>";
-    echo "<a href =\"login.php\" class =\"btn btn-default\">Profile Page</a>";
+    echo "<a href =\"login.php\" class =\"btn btn-default\">Log In</a>";
 } else {
     echo "<h1>Oops!<small> That username has been taken. Please try new one.</small></h1>";
     echo "<a href =\"register.php\" class =\"btn btn-default\">Try again</a>";
